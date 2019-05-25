329732a0d7a0f8c5dac32a4532015e32fe4f346b
[S] Artur Chabera <artur.chabera@snow.dog>
2019-05-25 10:37:49 +0200

    Initial Commit

---
+++ SkeletonSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonSample
+//
+//  Created by Artur Chabera on 25/05/2019.
+//  Copyright © 2019 Artur Chabera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
